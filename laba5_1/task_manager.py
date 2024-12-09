380ca95344cc137466170566ba9516510adc54b9
Sofia Popkova <sofiaprezident@gmail.com>
2024-12-09 17:43:25 +0300

    Добавлен функционал управления задачами

--- laba5_1/task_manager.py
+++ laba5_1/task_manager.py
@@ -1,3 +1,9 @@
 def create_task(title, description):
+    """Создает новую задачу и добавляет ее в список задач."""
+    task = {
+        'title': title,
+        'description': description
+    }
+    tasks.append(task)
     # Логика создания задачи
     print(f"Создана новая задача: {title}")
